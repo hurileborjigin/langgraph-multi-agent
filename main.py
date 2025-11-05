82386c637ea5b8145e35b644a3e3304981e88bda
hurileborjigin <heshigtenhurile@tum.de>
2025-11-05 22:10:40 +0100

    building graph and compling

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from langgraph-multi-agent!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ stock-market/flow.py
@@ -0,0 +1,56 @@
+# Bring in dependencies
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_ollama import ChatOllama
+from langchain_openai import AzureChatOpenAI
+from colorama import Fore
+from dotenv import load_dotenv
+load_dotenv()
+
+# Create LLM
+llm = AzureChatOpenAI(azure_deployment='gpt-4o')
+
+# Create State
+class State(dict):
+    messages: Annotated[list, add_messages]
+
+
+# Build llm node
+def chatbot(state: State):
+    # Get LLM response
+    response = llm.invoke(state['messages'])
+    # Return response in correct message format
+    return {"messages": [{"role": "assistant", "content": response.content}]}
+
+# Assemble a graph
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+# Add memory and compile graph
+memory = InMemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+
+# Build call loop and run it
+if __name__ == "__main__":
+    while True:
+        prompt = input("\n🤖Input your prompt:")
+        res = graph.invoke({
+            "messages": [{
+                "role": "user",
+                "content": prompt
+                }]},
+            config={
+                "configurable": 
+                    {
+                        "thread_id": 1234
+                        }
+                    }
+            )
+        print(Fore.LIGHTYELLOW_EX + res['messages'][-1].content + Fore.RESET)
+
+
