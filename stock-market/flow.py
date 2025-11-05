bcf3cf2afc410ad2a9a6b8527aea948a7d69a624
hurileborjigin <heshigtenhurile@tum.de>
2025-11-05 23:01:05 +0100

    agent implementation integrated tool call

--- stock-market/flow.py
+++ stock-market/flow.py
@@ -8,11 +8,22 @@ from langchain_openai import AzureChatOpenAI
 from colorama import Fore
 from dotenv import load_dotenv
 
+from langgraph.prebuilt import ToolNode
+from tool import simple_screener
+
 load_dotenv()
 
 # Create LLM
 llm = AzureChatOpenAI(azure_deployment="gpt-4o")
 
+# Create tool
+tools = [simple_screener]
+
+# Bind the llm with tools
+llm_with_tools = llm.bind_tools(tools)
+
+# Create tool node
+tool_node = ToolNode(tools)
 
 # Create State
 class State(dict):
@@ -22,20 +33,34 @@ class State(dict):
 # Build llm node
 def chatbot(state: State):
     # Get LLM response
-    response = llm.invoke(state["messages"])
+    response = llm_with_tools.invoke(state["messages"])
     # Return response in correct message format
-    return {"messages": [{"role": "assistant", "content": response.content}]}
+    return {"messages": [response]}
 
+
+# Create router node
+def router(state: State):
+    last_message = state['messages'][-1]
+    if hasattr(last_message, "tool_calls") and last_message.tool_calls:
+        return "tools"
+    else:
+        return END
 
 # Assemble a graph
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
 graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_conditional_edges("chatbot", router)
+
+# graph_builder.add_edge("chatbot", END)
 
 # Add memory and compile graph
 memory = InMemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
+
+
 
 
 # Build call loop and run it
